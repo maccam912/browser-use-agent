0762126c56c4ceb46b672ce071ba5d32cfff04c4
Matt Koski <maccam912@gmail.com>
2025-03-04 22:30:46 -0600

    use openrouter

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import asyncio
 import os
 import sys
 
-#llm = ChatOpenAI(model="google/gemini-2.0-flash-001", base_url="https://openrouter.ai/api/v1", api_key=os.getenv("OPENROUTER_API_KEY"))
-llm = ChatOpenAI(model="gpt-4o-mini")
+llm = ChatOpenAI(model="openai/gpt-4o-mini", base_url="https://openrouter.ai/api/v1", api_key=os.getenv("OPENROUTER_API_KEY"))
+# llm = ChatOpenAI(model="gpt-4o-mini")
 
 async def main():
     # Use command line argument if provided, otherwise use default
