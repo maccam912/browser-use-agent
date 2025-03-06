73938f6f98f04835443d54feb1cf28af07db0615
Matt Koski <maccam912@gmail.com>
2025-03-05 19:37:22 -0600

    test initial actions

--- main.py
+++ main.py
@@ -12,6 +12,12 @@ planner_llm = ChatOpenAI(model="google/gemini-2.0-pro-exp-02-05:free", base_url=
 # llm = ChatOpenAI(model="openai/gpt-4o-mini", base_url="https://openrouter.ai/api/v1", api_key=os.getenv("OPENROUTER_API_KEY"))
 # llm = ChatOpenAI(model="gpt-4o-mini")
 
+initial_actions = [
+	{'open_tab': {'url': 'https://www.google.com'}},
+	{'open_tab': {'url': 'https://en.wikipedia.org/wiki/Randomness'}},
+	{'scroll_down': {'amount': 1000}},
+]
+
 async def main():
     # Use command line argument if provided, otherwise use default
     task = sys.argv[1] if len(sys.argv) > 1 else "What is the meaning of life?"
@@ -19,6 +25,7 @@ async def main():
     agent = Agent(
         task=task,
         llm=llm,
+        initial_actions=initial_actions,
         planner_llm=planner_llm,
         planner_interval=4,
     )
