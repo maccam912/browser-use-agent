93933326e038900ec3432f6e67c17963494a69dc
Matt Koski <maccam912@gmail.com>
2025-03-12 22:17:07 -0500

    back to gpt-4o-mii

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ import logging
 
 logging.basicConfig(level=logging.ERROR)
 
-llm = ChatOpenAI(model="google/gemini-2.0-flash-001", base_url="https://openrouter.ai/api/v1", api_key=os.getenv("OPENROUTER_API_KEY"))
-# llm = ChatOpenAI(model="openai/gpt-4o-mini", base_url="https://openrouter.ai/api/v1", api_key=os.getenv("OPENROUTER_API_KEY"))
+llm = ChatOpenAI(model="openai/gpt-4o-mini", base_url="https://openrouter.ai/api/v1", api_key=os.getenv("OPENROUTER_API_KEY"))
 
 initial_actions = [
 	{'open_tab': {'url': 'https://www.picknsave.com'}},
